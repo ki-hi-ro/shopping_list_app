3041f76090c3eb47fd764470277c5762ae1dd118
ki-hi-ro <hiroki.hiroki@icloud.com>
2025-08-13 11:00:42 +0900

    起動できなくなった（フロントエンドがAPIに直接IPを指定しているからか？）

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f8c404e3'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ahjorvsqrcg"
+    "revision": "0.fln7u2sp0g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
